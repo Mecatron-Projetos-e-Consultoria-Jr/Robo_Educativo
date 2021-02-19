607eea0d093bb6db3e38b489bcb4956cd6ed3d6c
Erik Goto <erikgoto15@gmail.com>
2021-02-19 10:59:45 -0300

    ProgMan Execução - Contagem regressiva

--- Programação/Convertido_para_ino/src/PlacaSemaforo_Teste/PlacaSemaforo_Teste.ino
+++ Programação/Convertido_para_ino/src/PlacaSemaforo_Teste/PlacaSemaforo_Teste.ino
@@ -1,85 +1,59 @@
-#define pin1 2
-#define pin2 3
-#define pin3 4
-#define pin4 5
-#define pin5 6
-#define pin6 7
+#define pin1 1
+#define pin2 2
+#define pin3 3
+#define pin4 4
+#define pin5 5
+#define pin6 6
+#define pin7 7
+#define pin8 8
+#define pin9 9
+#define pin10 10
+#define pin11 11
+#define pin12 12
+#define pin13 13
 
-void setup() {
+void setup()
+{
   pinMode(pin1, OUTPUT);
   pinMode(pin2, OUTPUT);
   pinMode(pin3, OUTPUT);
   pinMode(pin4, OUTPUT);
   pinMode(pin5, OUTPUT);
   pinMode(pin6, OUTPUT);
+pinMode(pin7, OUTPUT);
+  pinMode(pin8, OUTPUT);
+  pinMode(pin9, OUTPUT);
+  pinMode(pin10, OUTPUT);
+  pinMode(pin11, OUTPUT);
+  pinMode(pin12, OUTPUT);
+  pinMode(pin13, OUTPUT);
 
+
+  Serial.begin(9600);
 }
 
-void loop() {
+void liga(int pin_liga, int pin_desliga){
+  digitalWrite(pin_liga, HIGH);
+  digitalWrite(pin_desliga, LOW);
 
-  for (int i = 0; i < 3; i++) {
-    pinMode(pin1, LOW);
-    pinMode(pin2, LOW);
-    pinMode(pin3, LOW);
-    pinMode(pin4, LOW);
-    pinMode(pin5, LOW);
-    pinMode(pin6, LOW);
-
-    delay(1000);
-
-    pinMode(pin1, HIGH);
-    pinMode(pin2, LOW);
-    pinMode(pin3, LOW);
-    pinMode(pin4, LOW);
-    pinMode(pin5, HIGH);
-    pinMode(pin6, LOW);
-
-    delay(1000);
-
-    pinMode(pin1, LOW);
-    pinMode(pin2, HIGH);
-    pinMode(pin3, LOW);
-    pinMode(pin4, LOW);
-    pinMode(pin5, LOW);
-    pinMode(pin6, HIGH);
-
-
-    delay(1000);
-
-    pinMode(pin1, LOW);
-    pinMode(pin2, LOW);
-    pinMode(pin3, HIGH);
-    pinMode(pin4, HIGH);
-    pinMode(pin5, LOW);
-    pinMode(pin6, LOW);
-
-    delay(1000);
-  }
-
-  pinMode(pin1, HIGH);
-  pinMode(pin2, LOW);
-  pinMode(pin3, LOW);
-  pinMode(pin4, HIGH);
-  pinMode(pin5, LOW);
-  pinMode(pin6, LOW);
-
+  Serial.println("Led Ligado: " + pin_liga);
   delay(1000);
+}
 
-  pinMode(pin1, LOW);
-  pinMode(pin2, HIGH);
-  pinMode(pin3, LOW);
-  pinMode(pin4, LOW);
-  pinMode(pin5, LOW);
-  pinMode(pin6, HIGH);
-
-  delay(1000);
-
-  pinMode(pin1, LOW);
-  pinMode(pin2, LOW);
-  pinMode(pin3, HIGH);
-  pinMode(pin4, LOW);
-  pinMode(pin5, HIGH);
-  pinMode(pin6, LOW);
-
-  delay(1000);
+void loop()
+{
+  liga(pin1, pin13);
+  liga(pin2, pin1);
+  liga(pin3, pin2);
+  liga(pin4, pin3);
+  liga(pin5, pin4);
+  liga(pin6, pin5);
+  liga(pin7, pin6);
+  liga(pin8, pin7);
+  liga(pin9, pin8);
+  liga(pin10, pin9);
+  liga(pin11, pin10);
+  liga(pin12, pin11);
+  liga(pin13, pin12);
+  
 }
--- Programação/ESP32/src/ProgramacaoManual.cpp
+++ Programação/ESP32/src/ProgramacaoManual.cpp
@@ -1,6 +1,17 @@
 #include "Arduino.h"
 #include "ProgramacaoManual.h"
 
+#include "Nextion.h"
+
+NexNumber linha = NexNumber(10, 4, "n0");
+NexText sensor = NexText(10, 5, "t1");
+NexText estado = NexText(10, 6, "t2");
+NexText delayTxt = NexText(10, 7, "t3");
+NexProgressBar progressBar = NexProgressBar(10, 3, "j1");
+
+NexPage page = NexPage(3, 0, "p_prog_man");
+char buffer3[100] = {0};
+
 ProgramacaoManual::ProgramacaoManual()
 {
     for (int i = 0; i < 45; i++)
@@ -15,19 +26,53 @@ void ProgramacaoManual::executar()
     pinMode(2, OUTPUT); //LED
     pinMode(4, OUTPUT); //Buzzer
 
+    int linhasTotais = 0;
+    while (matrix[linhasTotais][0] != 0)
+    {
+        linhasTotais++;
+    }
+
     //Executa os comandos da matriz matrix
     int i = 0;
+    int currentPBValue = 0;
     while (matrix[i][0] != 0)
     {
-        if (matrix[i][0] == -1)
+        if (matrix[i][0] == -1) //Comando -1 significa delay de x segundos
         {
-            delay(matrix[i][2]);
+            int cont = matrix[i][2] / 1000;
+            for (int j = 0; j <= matrix[i][2] / 1000; j++)
+            {
+                itoa(cont, buffer3, 10);
+                delayTxt.setText(buffer3);
+                delay(1000);
+                cont--;
+            }
         }
         else
         {
-            digitalWrite(matrix[i][0], matrix[i][2]);
+            digitalWrite(matrix[i][0], matrix[i][2]); //matrix[i][0] = numero do pino, matrix[i][2] = HIGH/LOW
+
+            linha.setValue(i);
+            if (matrix[i][0] == 2)
+            {
+                sensor.setText("LED");
+            }
+            else
+                sensor.setText("BUZZER");
+
+            if (matrix[i][2] == 1)
+            {
+                estado.setText("Ligado");
+            }
+            else
+                estado.setText("Desligado");
         }
+        currentPBValue = currentPBValue + 100 / linhasTotais;
+        progressBar.setValue(currentPBValue);
+
         i++;
     }
+    progressBar.setValue(100);
+    delay(500);
+    page.show();
 }
-//todo (Re)Desabilita o rabisco de erro
